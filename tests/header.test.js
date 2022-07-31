1df3252f8a78c3545ca0b0adc8a1cdfa8a982d58
Hein Lin Aung <heinlinaung6891@gmail.com>
2022-07-31 16:31:26 +0630

    HeaderTest: click the ahref and check the google url

--- tests/header.test.js
+++ tests/header.test.js
@@ -11,7 +11,13 @@ afterEach(async () => {
   await browser.close()
 })
 
-test('We can launch a browser', async () => {
+test('The header has correct text', async () => {
   const text = await page.$eval('a.brand-logo', el => el.innerHTML)
   expect(text).toEqual('Blogster')
+})
+
+test('clicking login to start oauth flow', async () => {
+  await page.click('.right a')
+  const url = await page.url()
+  expect(url).toMatch(/accounts\.google\.com/)
 })
