841093bdec52ac2d3dc71cf2d0d245cbe338e217
Hein Lin Aung <heinlinaung6891@gmail.com>
2022-07-31 16:12:57 +0630

    Updated : header.test (checking innerHTML text == 'Blogster')

--- tests/header.test.js
+++ tests/header.test.js
@@ -10,4 +10,7 @@ test('We can launch a browser', async () => {
   const browser = await puppeteer.launch({ headless: false })
   const page = await browser.newPage()
   await page.goto('localhost:3000')
+
+  const text = await page.$eval('a.brand-logo', el => el.innerHTML)
+  expect(text).toEqual('Blogster')
 })
