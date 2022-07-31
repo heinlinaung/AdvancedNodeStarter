1852d9b453b20bc5da26cc82fcf81f5928b49fa8
Hein Lin Aung <heinlinaung6891@gmail.com>
2022-07-31 19:31:10 +0630

    JestTest: prepare cookie(session, sig) to fake auth

--- tests/header.test.js
+++ tests/header.test.js
@@ -21,3 +21,19 @@ test('clicking login to start oauth flow', async () => {
   const url = await page.url()
   expect(url).toMatch(/accounts\.google\.com/)
 })
+
+test('when sign in, shows log out button', async () => {
+  const id = '62e4bd6960838031077a53d3'
+  const Buffer = require('safe-buffer').Buffer
+  const sessionObject = {
+    passport: { user: id }
+  }
+  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString('base64')
+
+  const Keygrip = require('keygrip')
+  const keys = require('../config/keys')
+  const keygrip = new Keygrip([keys.cookieKey])
+  const sig = keygrip.sign('session=' + sessionString)
+  console.log(sessionString)
+  console.log(sig)
+})
