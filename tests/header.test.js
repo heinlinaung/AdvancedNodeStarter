4d01ff60188972306f1d67e30f7159910a3dc732
Hein Lin Aung <heinlinaung6891@gmail.com>
2022-07-31 16:20:57 +0630

    Test:Using beforeEach to invoke browser and page

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,16 +1,12 @@
 const puppeteer = require('puppeteer')
-
-test('Adds two numbers', () => {
-  const sum = 1 + 2
-
-  expect(sum).toEqual(3)
+let browser, page
+beforeEach(async () => {
+  browser = await puppeteer.launch({ headless: false })
+  page = await browser.newPage()
+  await page.goto('localhost:3000')
 })
 
 test('We can launch a browser', async () => {
-  const browser = await puppeteer.launch({ headless: false })
-  const page = await browser.newPage()
-  await page.goto('localhost:3000')
-
   const text = await page.$eval('a.brand-logo', el => el.innerHTML)
   expect(text).toEqual('Blogster')
 })
