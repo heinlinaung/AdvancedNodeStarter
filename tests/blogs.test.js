ce50f9c2578970356127cb1f35b317153f69717a
Hein Lin Aung <heinlinaung6891@gmail.com>
2022-08-11 20:25:48 +0630

    JestTest : checking blogs form error on invalid inputs

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -10,9 +10,28 @@ afterEach(async () => {
   await page.close()
 })
 
-test('When logged in, can see blog create form', async () => {
-  await page.login()
-  await page.click('a.btn-floating')
-  const label = await page.getContentOf('form label')
-  expect(label).toEqual('Blog Title')
+describe('When logged in', async () => {
+  beforeEach(async () => {
+    await page.login()
+    await page.click('a.btn-floating')
+  })
+
+  test('Can see blog create form', async () => {
+    const label = await page.getContentOf('form label')
+    expect(label).toEqual('Blog Title')
+  })
+
+  describe('And using invalid inputs', async () => {
+    beforeEach(async () => {
+      await page.click('form button')
+    })
+
+    test('the form show error message', async () => {
+      const titleError = await page.getContentOf('.title .red-text')
+      const contentError = await page.getContentOf('.content .red-text')
+
+      expect(titleError).toEqual('You must provide a value')
+      expect(contentError).toEqual('You must provide a value')
+    })
+  })
 })
